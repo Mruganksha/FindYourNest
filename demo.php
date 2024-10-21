0d93acdb6c31e59308d08b9dae51f8e0cd02cb91
Mruganksha <kudakemruganksha@gmail.com>
2024-10-21 17:32:31 +0530

    registration style

--- demo.php
+++ demo.php
@@ -7,16 +7,16 @@
 	$number = $_POST['number'];
 
 	// Database connection
-	$conn = new mysqli('localhost','root','','test');
+	$conn = new mysqli('localhost','root','','tests');
 	if($conn->connect_error){
 		echo "$conn->connect_error";
 		die("Connection Failed : ". $conn->connect_error);
 	} else {
-		$stmt = $conn->prepare("insert into registration(firstName, lastName, gender, email, password, number) values(?, ?, ?, ?, ?, ?)");
+		$stmt = $conn->prepare("insert into registrations(firstName, lastName, gender, email, password, number) values(?, ?, ?, ?, ?, ?)");
 		$stmt->bind_param("sssssi", $firstName, $lastName, $gender, $email, $password, $number);
 		$execval = $stmt->execute();
 		echo $execval;
-		echo "Registration successfully...";
+		echo "registrations successfully...";
 		$stmt->close();
 		$conn->close();
 	}
